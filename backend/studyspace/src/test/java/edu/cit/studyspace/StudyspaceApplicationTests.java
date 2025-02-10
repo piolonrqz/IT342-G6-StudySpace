78ba75279f0797640b305394b804eac7d61c3d6f
LAB14_USER ON WS25 <L14X18W25@citulab.net>
2025-02-10 19:16:16 +0800

    initial setup for backend

---
+++ backend/studyspace/src/test/java/edu/cit/studyspace/StudyspaceApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.cit.studyspace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyspaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
